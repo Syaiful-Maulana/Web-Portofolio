95cd3e00dcf9bbde13376938eb01fd7b3b0b264a
Muhammad Syaiful Maulana <59109706+Syaiful-Maulana@users.noreply.github.com>
2025-09-14 20:48:18 +0700

    Update data.ts

--- src/components/About/data.ts
+++ src/components/About/data.ts
@@ -12,9 +12,7 @@ const getAge = (dateString: string) => {
 export const about = {
   picture: '/avatar.jpg',
   description: [
-    `I live in Kudus, Indonesia. I was born in Kudus, 4 Juni 2001. I'm ${getAge(
-      '2001-06-04'
-    )} years old. I'm an Informatics Student from Universitas Muria Kudus. `,
+    `I live in Kudus, Indonesia. I'm an Informatics Student from Universitas Muria Kudus. `,
     'My main tech stack is Golang,Node.js, Laravel, PostgreSQL, MySQL, Redis, and MongoDB',
     'Experienced Software Engineer with extensive skills in both backend and frontend technologies, specializing in GO, PHP, JavaScript, Laravel, Node.js, SQL, and NoSQL. Adept at developing and optimizing high-performance software applications. Possess strong frontend capabilities in HTML, CSS, JavaScript, and jQuery, with a proven ability to create engaging and responsive user interfaces. Currently focusing on advancing my career in backend and web development.',
   ],
